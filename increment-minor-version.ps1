fc66e639ed1833da77b0c547d362fd860560feac
Mathias Colpaert <colpaert.mathias@gmail.com>
2017-01-30 18:56:13 +0100

    increment nuget version

--- increment-minor-version.ps1
+++ increment-minor-version.ps1
@@ -1,16 +1,65 @@
-$nuspec = "C:\Users\MathiasColpaert\Source\Repos\matisco\Matisco.WebApi.Client\Matisco.WebApi.Client.nuspec"
+Function IncrementNuspecVersion
+{
+	Param(
+	[Parameter(Mandatory=$True)]
+	[string] $nuspecPath)
 
-$text = [System.IO.File]::ReadAllText($nuspec)
-$rxVersionTag = New-Object System.Text.RegularExpressions.Regex "\<version\>(?<Version>\d+\.\d+\.\d+)\</version\>", SingleLine
-$version = $rxVersionTag.Match($text).Groups["Version"].Value
+	BEGIN
+	{
+		$version = GetNuspecVersion($nuspecPath)
+		$newVersion = IncrementVersion($version)
+		SetNuspecVersion $nuspecPath $newVersion
+	}
+}
 
-Write-Host $version
-$majorMinor = $version.SubString(0, $version.LastIndexOf(".") + 1)
-$revision = $version.SubString($version.LastIndexOf(".") + 1)
-$rev = ([int]::Parse($revision) + 1)
+Function GetNuspecVersion
+{
+	Param(
+	[Parameter(Mandatory=$True)]
+	[string] $nuspecPath)
 
-$newVersion = [string]$majorMinor + $rev
-Write-Host $newVersion
+	BEGIN
+	{
+		$text = [System.IO.File]::ReadAllText($nuspecPath)
+		$rxVersionTag = New-Object System.Text.RegularExpressions.Regex "\<version\>(?<Version>\d+\.\d+\.\d+)\</version\>", SingleLine
+		$version = $rxVersionTag.Match($text).Groups["Version"].Value
 
-$text = $rxVersionTag.Replace($text, "<version>" + $newVersion + "</version>")
-[System.IO.File]::WriteAllText($nuspec, $text)
+		RETURN $version;
+	}
+}
+
+Function SetNuspecVersion
+{
+	Param(
+	[Parameter(Mandatory=$True)]
+	[string] $nuspecPath, 
+	[Parameter(Mandatory=$True)]
+	[string] $newVersion
+	)
+
+	BEGIN
+	{
+		$text = [System.IO.File]::ReadAllText($nuspecPath)
+		$rxVersionTag = New-Object System.Text.RegularExpressions.Regex "\<version\>(?<Version>\d+\.\d+\.\d+)\</version\>", SingleLine
+
+		$text = $rxVersionTag.Replace($text, "<version>" + $newVersion + "</version>")
+		[System.IO.File]::WriteAllText($nuspecPath, $text)
+	}
+}
+
+Function IncrementVersion
+{
+	Param(
+	[Parameter(Mandatory=$True)]
+	[string] $version)
+
+	BEGIN
+	{
+		$majorMinor = $version.SubString(0, $version.LastIndexOf(".") + 1)
+		$revision = $version.SubString($version.LastIndexOf(".") + 1)
+		$rev = ([int]::Parse($revision) + 1)
+		$newVersion = [string]$majorMinor + $rev
+
+		RETURN $newVersion;
+	}
+}
